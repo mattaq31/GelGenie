515f7d92da331136bc7eed1612533c3a81fe2293
Filip <f.busic@sms.ed.ac.uk>
2021-06-23 15:38:00 +0100

    Added ability to open image in GUI
    Currently works with .jpg but not .tif

--- gui/main.js
+++ gui/main.js
@@ -2,7 +2,28 @@
 $(document).ready(function() {
     $('.open').tooltip({trigger: 'hover', title: 'Open image', placement: 'bottom'});
     $('.find').tooltip({trigger: 'hover', title: 'Find bands', placement: 'bottom'});
+    // TO DO: Add the rest of the tooltips
 });
 
-// Open an image when button is clicked
-$('.open').on('click', function () { /* do stuff */ });
+// Open the image
+function previewFile() {
+  var preview = document.querySelector('img');
+  var file    = document.querySelector('input[type=file]').files[0];
+  var reader  = new FileReader();
+
+  reader.onloadend = function () {
+    preview.src = reader.result;
+  }
+
+  if (file) {
+    reader.readAsDataURL(file);
+    
+    var loadImgCont = document.getElementById("loadImgCont");
+    loadImgCont.style.display = "none";  // Hide the select image container
+    loadedImg.style.display = "block";   // Show the container with image
+  } else {
+    preview.src = "";
+  }
+}
+
+
