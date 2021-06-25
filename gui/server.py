c4fe347964afedb15605e4ad23839059e3dfa360
Filip <f.busic@sms.ed.ac.uk>
2021-06-25 17:35:39 +0100

    Added python server using socket.io and aiohttp
    Basic client server communication is functional (can open a gel image). Need to add the find bands functionality to GUI next.

---
+++ gui/server.py
@@ -0,0 +1,23 @@
+from aiohttp import web
+import socketio
+
+# Create an async aiohttp server
+sio = socketio.AsyncServer()
+app = web.Application()
+sio.attach(app)
+
+# Define aiohttp endpoints
+# This will deliver the main.html file to the client once connected.
+async def index(request):
+    with open('main.html') as f:
+        return web.Response(text=f.read(), content_type='html')
+
+# Bind our aiohttp endpoint to our app router
+app.router.add_get('/', index)
+
+# Add the static css and js files to the app router
+app.router.add_static('/', 'static')
+
+# We kick off our server
+if __name__ == '__main__':
+    web.run_app(app)
--- gui/static/main.js
+++ gui/static/main.js
@@ -26,4 +26,26 @@ function previewFile() {
   }
 }
 
+/*
+// Socket io
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+  allowEIO3: true // false by default
+});
+const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
+*/
